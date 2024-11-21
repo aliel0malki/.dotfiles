36f153831282bd319bacbeadb28d84c700375723
ali elmalki <alielmalki.developer@gamil.com>
2024-11-21 22:29:49 +0000

    UPDATE

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -13,16 +13,17 @@ vim.opt.shiftwidth = 4
 vim.opt.expandtab = false
 vim.opt.smartindent = true
 vim.opt.autoindent = true
+vim.opt.undofile = true
+vim.opt.undodir = vim.fn.stdpath("data") .. "/undodir"
+vim.opt.swapfile = false
+vim.opt.updatetime = 50
 vim.opt.clipboard = "unnamedplus"
 vim.opt.splitbelow = true
 vim.opt.splitright = true
 vim.opt.termguicolors = true
 vim.opt.scrolloff = 8
 vim.opt.sidescrolloff = 8
-vim.cmd([[
-	colorscheme retrobox
-]])
-
+vim.o.background = "dark"
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.uv.fs_stat(lazypath) then
 	vim.fn.system({
@@ -38,6 +39,32 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
 	{
+		"ellisonleao/gruvbox.nvim",
+		priority = 1000,
+		config = function()
+			-- Default options:
+			require("gruvbox").setup({
+				terminal_colors = true, -- add neovim terminal colors
+				undercurl = true,
+				underline = true,
+				bold = false,
+				italic = {
+					strings = false,
+					emphasis = false,
+					comments = false,
+					operators = false,
+					folds = true,
+				},
+			})
+			vim.cmd("colorscheme gruvbox")
+		end,
+	},
+	{
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		config = true,
+	},
+	{
 		"nvim-treesitter/nvim-treesitter",
 		build = ":TSUpdate",
 		dependencies = {
@@ -48,7 +75,9 @@ require("lazy").setup({
 			local config = require("nvim-treesitter.configs")
 			config.setup({
 				auto_install = true,
-				highlight = { enable = true },
+				highlight = {
+					enable = true,
+				},
 				indent = { enable = true },
 				autotag = {
 					enable = true,
@@ -64,6 +93,23 @@ require("lazy").setup({
 				},
 				require("template-string").setup({}),
 			})
+		end,
+	},
+	{
+		"ej-shafran/compile-mode.nvim",
+		tag = "v5.3.2",
+		branch = "nightly",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			{ "m00qek/baleia.nvim", tag = "v1.3.0" },
+		},
+		config = function()
+			---@type CompileModeOpts
+			vim.g.compile_mode = {
+				baleia_setup = true,
+			}
+			vim.keymap.set("n", "<leader>cc", ":Compile ")
+			vim.keymap.set("n", "<leader>cne", ":NextError<CR>")
 		end,
 	},
 	{
@@ -149,28 +195,31 @@ require("lazy").setup({
 	},
 })
 
-vim.api.nvim_create_autocmd("LspAttach", {
-	callback = function(event)
-		local opts = { buffer = event.buf }
+local lsp_zero = require("lsp-zero")
 
-		vim.keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<cr>", opts)
-		vim.keymap.set("n", "C-k", "<cmd>lua vim.diagnostic.open_float()<cr>", opts)
-		vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<cr>", opts)
-		vim.keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<cr>", opts)
-		vim.keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<cr>", opts)
-		vim.keymap.set("n", "go", "<cmd>lua vim.lsp.buf.type_definition()<cr>", opts)
-		vim.keymap.set("n", "gr", "<cmd>lua vim.lsp.buf.references()<cr>", opts)
-		vim.keymap.set("n", "gs", "<cmd>lua vim.lsp.buf.signature_help()<cr>", opts)
-		vim.keymap.set("n", "<F2>", "<cmd>lua vim.lsp.buf.rename()<cr>", opts)
-		vim.keymap.set({ "n", "x" }, "<F3>", "<cmd>lua vim.lsp.buf.format({async = true})<cr>", opts)
-		vim.keymap.set("n", "<F4>", "<cmd>lua vim.lsp.buf.code_action()<cr>", opts)
-		print("LSP attached: " .. vim.inspect(event))
-	end,
+local lsp_attach = function(client, bufnr)
+	local opts = { buffer = bufnr }
+
+	vim.keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<cr>", opts)
+	vim.keymap.set("n", "<C-k>", "<cmd>lua vim.diagnostic.open_float()<cr>", opts)
+	vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<cr>", opts)
+	vim.keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<cr>", opts)
+	vim.keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<cr>", opts)
+	vim.keymap.set("n", "go", "<cmd>lua vim.lsp.buf.type_definition()<cr>", opts)
+	vim.keymap.set("n", "gr", "<cmd>lua vim.lsp.buf.references()<cr>", opts)
+	vim.keymap.set("n", "gs", "<cmd>lua vim.lsp.buf.signature_help()<cr>", opts)
+	vim.keymap.set("n", "<F2>", "<cmd>lua vim.lsp.buf.rename()<cr>", opts)
+	vim.keymap.set({ "n", "x" }, "<F3>", "<cmd>lua vim.lsp.buf.format({async = true})<cr>", opts)
+	vim.keymap.set("n", "ca", "<cmd>lua vim.lsp.buf.code_action()<cr>", opts)
+	print("Attached " .. client.name)
+end
+
+lsp_zero.extend_lspconfig({
+	sign_text = true,
+	lsp_attach = lsp_attach,
+	float_border = "rounded",
+	capabilities = require("cmp_nvim_lsp").default_capabilities(),
 })
-
-local lspconfig_defaults = require("lspconfig").util.default_config
-lspconfig_defaults.capabilities =
-	vim.tbl_deep_extend("force", lspconfig_defaults.capabilities, require("cmp_nvim_lsp").default_capabilities())
 
 require("mason").setup({})
 require("mason-lspconfig").setup({
@@ -182,6 +231,7 @@ require("mason-lspconfig").setup({
 })
 
 local cmp = require("cmp")
+local cmp_action = lsp_zero.cmp_action()
 
 require("luasnip.loaders.from_vscode").lazy_load()
 
@@ -189,11 +239,8 @@ cmp.setup({
 	sources = {
 		{ name = "path" },
 		{ name = "nvim_lsp" },
-		{ name = "luasnip", keyword_length = 2 },
+		{ name = "luasnip", keyword_length = 3 },
 		{ name = "buffer", keyword_length = 3 },
-	},
-	performance = {
-		max_view_entries = 20,
 	},
 	snippet = {
 		expand = function(args)
@@ -201,26 +248,11 @@ cmp.setup({
 		end,
 	},
 	mapping = cmp.mapping.preset.insert({
-		["<Enter>"] = cmp.mapping.confirm({ select = true }),
+		["<C-b>"] = cmp.mapping.scroll_docs(-4),
+		["<C-f>"] = cmp.mapping.scroll_docs(4),
 		["<C-Space>"] = cmp.mapping.complete(),
-		["<C-u>"] = cmp.mapping.scroll_docs(-4),
-		["<C-d>"] = cmp.mapping.scroll_docs(4),
-		["<C-f>"] = cmp.mapping(function(fallback)
-			local luasnip = require("luasnip")
-			if luasnip.locally_jumpable(1) then
-				luasnip.jump(1)
-			else
-				fallback()
-			end
-		end, { "i", "s" }),
-		["<C-b>"] = cmp.mapping(function(fallback)
-			local luasnip = require("luasnip")
-			if luasnip.locally_jumpable(-1) then
-				luasnip.jump(-1)
-			else
-				fallback()
-			end
-		end, { "i", "s" }),
+		["<C-e>"] = cmp.mapping.abort(),
+		["<CR>"] = cmp.mapping.confirm({ select = true }),
 	}),
 })
 
