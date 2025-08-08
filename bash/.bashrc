82cbc35a91aa989bb758935886e27ada9ebf44a9
ali. <alielmalki.developer@gmail.com>
2025-08-08 11:50:23 +0300

    hellyahbaby

--- bash/.bashrc
+++ bash/.bashrc
@@ -16,7 +16,7 @@ alias mkdir='mkdir -pv'
 alias mv='mv -i'
 alias cp='cp -i'
 alias rm='rm -rf'
-alias ls='ls -la --color=auto'
+alias ls='ls -a --color=auto'
 alias ln='ln -i'
 alias du='du -ch'
 alias emacs='emacs -nw'
@@ -26,13 +26,6 @@ export PATH=$PATH:/usr/local/go/bin
 export BUN_INSTALL=$HOME/.bun
 export PATH=$BUN_INSTALL/bin:$PATH
 export PATH=$PATH:~/.zig/
-
-# fnm setup (if installed)
-FNM_PATH="/home/alewa/.local/share/fnm"
-if [ -d "$FNM_PATH" ]; then
-  export PATH="/home/alewa/.local/share/fnm:$PATH"
-  eval "$(fnm env --use-on-cd --version-file-strategy=recursive)"
-fi
 
 alias tmux-sessionizer='~/tmux-sessionizer.sh'
 bind -x '"\C-f": tmux-sessionizer'
--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -24,191 +24,275 @@ vim.opt.termguicolors = true
 vim.opt.scrolloff = 8
 vim.opt.sidescrolloff = 8
 vim.o.background = "dark"
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.uv.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable",
-		lazypath,
-	})
+	vim.fn.system(
+		{ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath }
+	)
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({
+require("lazy").setup(
 	{
-		"ellisonleao/gruvbox.nvim",
-		priority = 1000,
-		config = function()
-			-- Default options:
-			require("gruvbox").setup({
-				terminal_colors = true, -- add neovim terminal colors
-				undercurl = true,
-				underline = true,
-				bold = false,
-				italic = {
-					strings = false,
-					emphasis = false,
-					comments = false,
-					operators = false,
-					folds = true,
-				},
-			})
-			vim.cmd("colorscheme gruvbox")
-		end,
-	},
-	{
-		"nvim-treesitter/nvim-treesitter",
-		build = ":TSUpdate",
-		dependencies = {
-			"windwp/nvim-ts-autotag",
-			"axelvc/template-string.nvim",
+		{
+			"doums/darcula",
+			priority = 1000,
+			config = function()
+				vim.cmd("colorscheme darcula")
+			end
 		},
-		config = function()
-			local config = require("nvim-treesitter.configs")
-			config.setup({
-				auto_install = true,
-				highlight = {
-					enable = true,
+		{
+			"nvim-treesitter/nvim-treesitter",
+			build = ":TSUpdate",
+			dependencies = { "windwp/nvim-ts-autotag", "axelvc/template-string.nvim" },
+			config = function()
+				require("nvim-treesitter.configs").setup(
+					{
+						auto_install = true,
+						highlight = { enable = true },
+						indent = { enable = true },
+						autotag = { enable = true },
+						incremental_selection = {
+							enable = true,
+							keymaps = {
+								init_selection = "<enter>",
+								node_incremental = "<enter>",
+								scope_incremental = false,
+								node_decremental = "<bs>"
+							}
+						}
+					}
+				)
+				require("template-string").setup({})
+			end
+		},
+		{
+			"ej-shafran/compile-mode.nvim",
+			branch = "latest",
+			dependencies = { "nvim-lua/plenary.nvim", { "m00qek/baleia.nvim", tag = "v1.3.0" } },
+			config = function()
+				vim.g.compile_mode = { baleia_setup = true, bang_expansion = true }
+				vim.keymap.set("n", "<leader>cc", ":Compile ")
+				vim.keymap.set("n", "<leader>cne", ":NextError<CR>")
+			end
+		},
+		{
+			"stevearc/conform.nvim",
+			config = function()
+				local conform = require("conform")
+				conform.setup({ formatters_by_ft = { lua = { "stylua" } } })
+				vim.keymap.set(
+					"n",
+					"<leader>fm",
+					function()
+						conform.format({ lsp_fallback = true, async = true, timeout_ms = 1000 })
+					end
+				)
+			end
+		},
+		{
+			"mason-org/mason.nvim",
+			opts = {},
+		},
+		{
+			"mason-org/mason-lspconfig.nvim",
+			dependencies = {
+				"mason-org/mason.nvim",
+				"neovim/nvim-lspconfig",
+			},
+			event = { "VeryLazy", "BufReadPre", "BufNewFile" },
+			opts = function(_)
+				local mr = require("mason-registry")
+				vim.g.formatters = {}
+				mr.refresh(function()
+					for _, tool in ipairs(vim.g.formatters) do
+						local p = mr.get_package(tool)
+						if not p:is_installed() then
+							p:install()
+						end
+					end
+				end)
+
+				return {
+					ensure_installed = vim.g.lsps,
+					automatic_enable = true,
+				}
+			end,
+		},
+		{
+			"folke/lazydev.nvim",
+			ft = "lua", -- only load on lua files
+			opts = {
+				library = {
+					-- See the configuration section for more details
+					-- Load luvit types when the `vim.uv` word is found
+					{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
 				},
-				indent = { enable = true },
-				autotag = {
-					enable = true,
-				},
-				incremental_selection = {
-					enable = true,
-					keymaps = {
-						init_selection = "<enter>",
-						node_incremental = "<enter>",
-						scope_incremental = false,
-						node_decremental = "<bs>",
+				sources = {
+					-- add lazydev to your completion providers
+					default = { "lazydev" },
+					providers = {
+						lazydev = {
+							name = "LazyDev",
+							module = "lazydev.integrations.blink",
+							score_offset = 100, -- show at a higher priority than lsp
+						},
 					},
 				},
-				require("template-string").setup({}),
-			})
-		end,
-	},
-	{
-		"ej-shafran/compile-mode.nvim",
-		tag = "v5.3.2",
-		branch = "nightly",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			{ "m00qek/baleia.nvim", tag = "v1.3.0" },
+			},
 		},
-		config = function()
-			vim.g.compile_mode = {
-				baleia_setup = true,
-			}
-			vim.keymap.set("n", "<leader>cc", ":Compile ")
-			vim.keymap.set("n", "<leader>cne", ":NextError<CR>")
-		end,
-	},
-	{
-		"stevearc/conform.nvim",
-		config = function()
-			local conform = require("conform")
-			conform.setup({
-				formatters_by_ft = {
-					lua = { "stylua" },
+		{
+			"saghen/blink.cmp",
+			dependencies = {
+				"rafamadriz/friendly-snippets",
+				"onsails/lspkind.nvim",
+			},
+			---@module 'blink.cmp'
+			---@type blink.cmp.Config
+			opts = {
+				fuzzy = { implementation = "rust" },
+				appearance = {
+					use_nvim_cmp_as_default = false,
+					nerd_font_variant = "mono",
 				},
-			})
-			vim.keymap.set("n", "<leader>fm", function()
-				conform.format({ lsp_fallback = true, async = true, timeout_ms = 1000 })
-			end)
-		end,
-	},
-	{
-		"Exafunction/codeium.vim",
-		config = function()
-			vim.keymap.set("i", "<Tab>", function()
-				return vim.fn["codeium#Accept"]()
-			end, { expr = true, silent = true })
-			vim.keymap.set("i", "<c-;>", function()
-				return vim.fn["codeium#CycleCompletions"](1)
-			end, { expr = true, silent = true })
-			vim.keymap.set("i", "<c-,>", function()
-				return vim.fn["codeium#CycleCompletions"](-1)
-			end, { expr = true, silent = true })
-			vim.keymap.set("i", "<c-x>", function()
-				return vim.fn["codeium#Clear"]()
-			end, { expr = true, silent = true })
-		end,
-	},
-	{
-		"neoclide/coc.nvim",
-		branch = "release",
-		config = function()
-			local keyset = vim.keymap.set
-			local opts = { silent = true, noremap = true, expr = true, replace_keycodes = false }
-			keyset("i", "<cr>", [[coc#pum#visible() ? coc#pum#confirm() : "\<C-g>u\<CR>\<c-r>=coc#on_enter()\<CR>"]],
-				opts)
-			keyset("i", "<c-l>", "<Plug>(coc-snippets-expand-jump)")
-			keyset("n", "[d", "<Plug>(coc-diagnostic-prev)", { silent = true })
-			keyset("n", "]d", "<Plug>(coc-diagnostic-next)", { silent = true })
-			keyset("n", "gd", "<Plug>(coc-definition)", { silent = true })
-			keyset("n", "gy", "<Plug>(coc-type-definition)", { silent = true })
-			keyset("n", "gi", "<Plug>(coc-implementation)", { silent = true })
-			keyset("n", "gr", "<Plug>(coc-references)", { silent = true })
-			keyset("n", "C-k", "<Plug>(coc-diagnostic-prev)", { silent = true })
-			function _G.show_docs()
-				local cw = vim.fn.expand('<cword>')
-				if vim.fn.index({ 'vim', 'help' }, vim.bo.filetype) >= 0 then
-					vim.api.nvim_command('h ' .. cw)
-				elseif vim.api.nvim_eval('coc#rpc#ready()') then
-					vim.fn.CocActionAsync('doHover')
-				else
-					vim.api.nvim_command('!' .. vim.o.keywordprg .. ' ' .. cw)
-				end
+
+				completion = {
+					accept = { auto_brackets = { enabled = true } },
+					documentation = {
+						auto_show = true,
+						auto_show_delay_ms = 250,
+						treesitter_highlighting = true,
+					},
+					menu = {
+						cmdline_position = function()
+							if vim.g.ui_cmdline_pos ~= nil then
+								local pos = vim.g.ui_cmdline_pos -- (1, 0)-indexed
+								return { pos[1] - 1, pos[2] }
+							end
+							local height = (vim.o.cmdheight == 0) and 1 or vim.o.cmdheight
+							return { vim.o.lines - height, 0 }
+						end,
+
+						draw = {
+							columns = {
+								{ "kind_icon", "label", gap = 1 },
+								{ "kind" },
+							},
+							components = {
+								kind_icon = {
+									text = function(item)
+										local kind = require("lspkind").symbol_map[item.kind] or ""
+										return kind .. " "
+									end,
+									highlight = "CmpItemKind",
+								},
+								label = {
+									text = function(item)
+										return item.label
+									end,
+									highlight = "CmpItemAbbr",
+								},
+								kind = {
+									text = function(item)
+										return item.kind
+									end,
+									highlight = "CmpItemKind",
+								},
+							},
+						},
+					},
+				},
+
+				keymap = {
+					["<C-k>"] = { "show", "show_documentation", "hide_documentation" },
+					["<CR>"] = { "accept", "fallback" },
+
+					["<Tab>"] = {
+						function(cmp)
+							return cmp.select_next()
+						end,
+						"snippet_forward",
+						"fallback",
+					},
+					["<S-Tab>"] = {
+						function(cmp)
+							return cmp.select_prev()
+						end,
+						"snippet_backward",
+						"fallback",
+					},
+
+					["<Up>"] = { "select_prev", "fallback" },
+					["<Down>"] = { "select_next", "fallback" },
+					["<C-p>"] = { "select_prev", "fallback" },
+					["<C-n>"] = { "select_next", "fallback" },
+					["<C-up>"] = { "scroll_documentation_up", "fallback" },
+					["<C-down>"] = { "scroll_documentation_down", "fallback" },
+				},
+
+				signature = {
+					enabled = true,
+				},
+
+				sources = {
+					default = { "lsp", "path", "snippets", "buffer" },
+					providers = {
+						lsp = {
+							min_keyword_length = 1, -- Number of characters to trigger porvider
+							score_offset = 0, -- Boost/penalize the score of the items
+						},
+						path = {
+							min_keyword_length = 0,
+						},
+						snippets = {
+							min_keyword_length = 2,
+						},
+						buffer = {
+							min_keyword_length = 3,
+							max_items = 5,
+						},
+					},
+				},
+			},
+		},
+		{
+			"ibhagwan/fzf-lua",
+			dependencies = { "nvim-tree/nvim-web-devicons" },
+			config = function()
+				require("fzf-lua").setup({})
+				vim.keymap.set("n", "<leader>ff", require("fzf-lua").files)
+				vim.keymap.set("n", "<leader>fg", require("fzf-lua").live_grep)
+				vim.keymap.set("n", "<leader>fb", require("fzf-lua").buffers)
 			end
+		},
+		{
+			"stevearc/oil.nvim",
+			dependencies = { "nvim-tree/nvim-web-devicons" },
+			config = function()
+				require("oil").setup()
+				vim.keymap.set("n", "<leader>e", ":Oil --float<CR>")
+			end
+		},
+		{
+			"TimUntersberger/neogit",
+			dependencies = "nvim-lua/plenary.nvim",
+			config = function()
+				require("neogit").setup()
+				vim.keymap.set("n", "<leader>ll", ":Neogit<CR>")
+			end
+		}
+	}
+)
 
-			keyset("n", "K", '<CMD>lua _G.show_docs()<CR>', { silent = true })
-			vim.api.nvim_create_augroup("CocGroup", {})
-			vim.api.nvim_create_autocmd("CursorHold", {
-				group = "CocGroup",
-				command = "silent call CocActionAsync('highlight')",
-				desc = "Highlight symbol under cursor on CursorHold"
-			})
-			keyset("n", "<leader>rn", "<Plug>(coc-rename)", { silent = true })
-			keyset("x", "<leader>fm", "<Plug>(coc-format-selected)", { silent = true })
-			-- Mappings for CoCList
-			-- code actions and coc stuff
-			---@diagnostic disable-next-line: redefined-local
-			local opts = { silent = true, nowait = true }
-			-- Show all diagnostics
-			keyset("n", "<leader>xx", ":<C-u>CocList diagnostics<cr>", opts)
-		end,
-	},
-	{
-		"ibhagwan/fzf-lua",
-		dependencies = { "nvim-tree/nvim-web-devicons" },
-		config = function()
-			require("fzf-lua").setup({})
-			vim.keymap.set("n", "<leader>ff", require("fzf-lua").files)
-			vim.keymap.set("n", "<leader>fg", require("fzf-lua").live_grep)
-			vim.keymap.set("n", "<leader>fb", require("fzf-lua").buffers)
-		end,
-	},
-	{
-		"stevearc/oil.nvim",
-		config = function()
-			require("oil").setup()
-			vim.keymap.set("n", "<leader>e", ":Oil --float<CR>")
-		end,
-	},
+-- Enable built-in LSP (example for LuaLS)
+vim.lsp.config(
+	"luals",
+	{ cmd = { "lua-language-server" }, filetypes = { "lua" }, root_markers = { ".luarc.json", ".luarc.jsonc" } }
+)
+vim.lsp.enable("luals")
 
-	{
-		"TimUntersberger/neogit",
-		dependencies = "nvim-lua/plenary.nvim",
-		config = function()
-			require("neogit").setup()
-			vim.keymap.set("n", "<leader>ll", ":Neogit<CR>")
-		end,
-	},
-})
-
+-- Keymaps
 vim.keymap.set("n", "<leader>v", ":vsplit<CR>")
 vim.keymap.set("n", "<leader>s", ":split<CR>")
 vim.keymap.set("n", "<Tab>", ":bnext<CR>")
@@ -218,16 +302,23 @@ vim.keymap.set("n", "<leader>j", ":tabprevious<CR>")
 vim.keymap.set("n", "<leader>k", ":tabnext<CR>")
 vim.keymap.set("n", "<C-j>", "<C-w>w")
 
-vim.api.nvim_create_autocmd("TextYankPost", {
-	callback = function()
-		vim.highlight.on_yank()
-	end,
-})
+-- AutoCmds
+vim.api.nvim_create_autocmd(
+	"TextYankPost",
+	{
+		callback = function()
+			vim.highlight.on_yank()
+		end
+	}
+)
 
-vim.api.nvim_create_autocmd("BufReadPost", {
-	callback = function()
-		vim.api.nvim_exec('silent! normal! g`"zv', false)
-	end,
-})
+vim.api.nvim_create_autocmd(
+	"BufReadPost",
+	{
+		callback = function()
+			vim.api.nvim_exec('silent! normal! g`"zv', false)
+		end
+	}
+)
 
 vim.keymap.set("n", "<leader><leader>", ":source ~/.config/nvim/init.lua<CR>")
